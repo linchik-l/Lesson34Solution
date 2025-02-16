ea5de3be28b561f8bb33e6663c5d4547ce1f1af4
linchic-l <lebedevskayalina@gmail.com>
2025-02-16 18:07:42 +0300

    Add project files.

---
+++ Project1/test01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//global
+//int c = 0;
+int test(int c) {
+	//local
+    //int c = 0;
+	c++;
+	cout << c << endl;
+	return c;
+}
+
+int main() {
+	int c = 0;
+	c = test(c);
+	c = test(c);
+	c = test(c);
+	cout << c << endl;
+	return 0;
+}
